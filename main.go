9c98f87d7545be2233b05b46f3ecf8ea77f5ff87
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-01-15 18:11:17 +0530

    Accept url flag

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello")
+	streamUrl := flag.String("url", "", "A logstream URL. Example: redis://localhost:6379/0/<stream_id>")
+	flag.Parse()
+
+	if *streamUrl == "" {
+		fmt.Println("Expected --url to be set!")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Streaming logs from/to %s\n", *streamUrl)
 }
